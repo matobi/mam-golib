c824c910a986769e85d35b365a2b96e0d52caf73
Mattias Tobiasson <mattias.tobiasson@nentgroup.com>
2019-04-12 16:00:20 +0200

    reduce logging

--- pkg/camunda/runner.go
+++ pkg/camunda/runner.go
@@ -37,7 +37,7 @@ func (c *Client) Run(taskChan chan<- Task) {
 		}
 
 		taskChan <- *task
-		timeNextPoll = time.Now().Add(5 * time.Second) // poll again if we found a task
+		timeNextPoll = time.Now() // poll again if we found a task
 	}
 	close(taskChan)
 }
@@ -45,7 +45,6 @@ func (c *Client) Run(taskChan chan<- Task) {
 // ProcessTasks Receives Tasks from channel and calls matching TaskHandlerFunc.
 func (c *Client) ProcessTasks(handlers map[string]TaskHandlerFunc, taskChan <-chan Task) {
 	for task := range taskChan {
-		c.lw.Info("processTasks begin", "key", task.BusinessKey)
 		handler, found := handlers[task.TopicName]
 		if !found {
 			c.lw.Err("no matching handler", "topic", task.TopicName)
@@ -64,7 +63,6 @@ func (c *Client) ProcessTasks(handlers map[string]TaskHandlerFunc, taskChan <-ch
 			}
 			continue
 		}
-		c.lw.Info("processTasks done", "key", task.BusinessKey)
 		c.lw.Info("bpmTaskDone", "externalId", externalID, "procdefKey", task.Key, "activityId", task.ActivityID, "topic", task.TopicName, "businessKey", task.BusinessKey)
 		if err := c.CompleteTask(task); err != nil {
 			c.lw.Err("could not complete camunda task", err, "externalId", externalID, "procdefKey", task.Key, "activityId", task.ActivityID, "topic", task.TopicName, "businessKey", task.BusinessKey)
